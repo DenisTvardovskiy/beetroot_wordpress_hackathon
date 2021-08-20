00925e75c64f56544c835fac2ba8817ddd848f19
Nodari Pylypyshak <32438959+nodari-dev@users.noreply.github.com>
2021-08-20 09:56:35 +0300

    starter theme upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package jwp
+ */
+
+?>
+
+<?php wp_footer(); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/index.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri()?>/assets/images/favicon.png"/>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/styles/dist/style.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/styles/dist/grid.min.css">
+	<?php wp_head(); ?>
+</head>
+
+<body>
+
+<?php wp_body_open(); ?>
+
+<h1>STARTER WORDPRESS THEME</h1>
