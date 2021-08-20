f4dde5f0bc3cdf176cf950eda34c7eb582c80832
DenisTvardovskiy <40178797+DenisTvardovskiy@users.noreply.github.com>
2021-08-20 20:07:02 +0300

    '.'

--- assets/js/data/gdp.js
+++ assets/js/data/gdp.js
@@ -24,3 +24,21 @@ var svgMapDataGPD = {
   applyData: 'gdpAdjusted',
   values: {}
 }
+// ${node.overview.map((item)=>{
+//   <li data-content="">${item}</li>
+// })}
+//
+// ${node.localRestrictionsPolicies.map((item)=>{
+//                     <li className="list-inline-item">
+//                       <div className="t-chip small mb-2 warning"
+//                            title=${item.description}>${item.title}
+// </div>
+// </li>
+// })}
+
+// ${node.whatToExpectInCountry.map((item)=>{
+// <div className="mb-4">
+//   <p className="font-weight-bold mb-0">${item.title}</p>
+// <p>${item.content}</p>
+// </div>
+// })}
