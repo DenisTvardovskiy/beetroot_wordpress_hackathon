0fea5c0603e9ce65c3da97cd996183b6f9129e0d
Denys <denis200358@gmail.com>
2021-08-29 20:51:44 +0300

    added path

---
+++ src/Components/Map/Map.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Map extends  React.Component<any, any>{
+    constructor(props:any) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return {
+            
+        };
+    }
+}
+
+export default Map
