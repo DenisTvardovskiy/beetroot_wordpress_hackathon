328692e6b95a28a8dcaf257b9a885e4155e2214f
nodari-dev <nodari.pylypyshak@gmail.com>
2021-08-30 23:51:10 +0300

    basic popup

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import './Grid.min.css';
+import Header from "../Header/Header";
+import Github from "../Github/Github";
+import MapLegend from "../MapLegend/MapLegend";
+import Test from "../Test/Test";
 
 function App() {
   return (
     <div className="App">
-     Lol
+        <Header/>
+        <Test/>
+        <MapLegend/>
+        <Github/>
     </div>
   );
 }
